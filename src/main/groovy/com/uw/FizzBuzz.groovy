82f6e7b5d4a91f180077cb9b39f1ab2ea20aacf4
Adam <foo@bar.com>
2016-07-05 16:12:06 +0100

    [adam,muz] initial commit for fizzbuzz

---
+++ src/main/groovy/com/uw/FizzBuzz.groovy
@@ -0,0 +1,37 @@
+package com.uw
+
+/**
+ * Created by uw on 05/07/16.
+ */
+class FizzBuzz {
+
+    def evaluate(def integers) {
+        integers.forEach {
+            switch (it) {
+                case { it % 3 == 0 && it % 5 == 0 }:
+                    println "fizzbuzz"
+                    break
+                case { it % 3 == 0 }:
+                    println "fizz"
+                    breakgi
+                case { it % 5 == 0 }:
+                    println "buzz"
+                    break
+                default:
+                    println it.toString()
+                    break
+            }
+
+//            if(it % 3 == 0 && it % 5 == 0){
+//                println "fizzbuzz"
+//            } else if(it % 3 == 0 ){
+//                println "fizz"
+//            } else if(it % 5 == 0 ){
+//                println "buzz"
+//            } else {
+//                println it.toString()
+//            }
+        }
+    }
+
+}
---
+++ src/test/groovy/com/uw/FizzBuzzSpec.groovy
@@ -0,0 +1,89 @@
+package com.uw
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * Created by uw on 05/07/16.
+ */
+class FizzBuzzSpec extends Specification {
+
+    @Unroll("Values not divisible by 3 or 5 are not changed where number is #number")
+    def "Values not divisible by 3 or 5 are not changed"(){
+        given:
+        def numberList = (1..100)
+        FizzBuzz fizzBuzz = new FizzBuzz()
+
+        when:
+        fizzBuzz.evaluate(numberList)
+
+        then:
+        sysOut.toString().split('\n')[number -1] == number.toString()
+
+        where:
+        number << (1..100).findAll{ (it % 3 != 0) && (it % 5 != 0) }
+    }
+
+    @Unroll("fizz is #number")
+    def "values divisible by 3 are printed as fizz"(){
+        given:
+        def numberlist = (1..100)
+        FizzBuzz fizzBuzz = new FizzBuzz()
+
+        when:
+        fizzBuzz.evaluate(numberlist)
+
+        then:
+        sysOut.toString().split('\n')[number -1] == "fizz"
+
+        where:
+        number << (1..100).findAll{ it % 3 == 0 && it % 5 != 0}
+    }
+
+    @Unroll("buzz is  #number")
+    def "values divisible by 5 are printed as buzz"(){
+        given:
+        def numberlist = (1..100)
+        FizzBuzz fizzBuzz = new FizzBuzz()
+
+        when:
+        fizzBuzz.evaluate(numberlist)
+
+        then:
+        sysOut.toString().split('\n')[number -1] == "buzz"
+
+        where:
+        number << (1..100).findAll{ it % 5 == 0 &&  it % 3 != 0 }
+    }
+
+    @Unroll("fizzbuzz  #number")
+    def "values divisible by 3 and 5 are printed as fizzbuzz"(){
+        given:
+        def numberlist = (1..100)
+        FizzBuzz fizzBuzz = new FizzBuzz()
+
+        when:
+        fizzBuzz.evaluate(numberlist)
+
+        then:
+        sysOut.toString().split('\n')[number -1] == "fizzbuzz"
+
+        where:
+        number << (1..100).findAll{ it % 5 == 0 &&  it % 3 == 0 }
+    }
+
+
+
+    private final ByteArrayOutputStream sysOut = new ByteArrayOutputStream();
+    private final PrintStream originalSysOut = System.out;
+
+
+    def setup(){
+        System.setOut(new PrintStream(sysOut));
+    }
+
+    def cleanup(){
+        System.setOut(originalSysOut);
+    }
+
+}
